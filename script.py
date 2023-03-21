1b841f78a15a1e72785e1d3531d28e1841da8e32
NajatN <121221757+NajatN@users.noreply.github.com>
2023-03-21 22:56:06 +0200

    Check number of arguments

--- script.py
+++ script.py
@@ -3,3 +3,11 @@ import requests
 import re
 import urllib.parse
 import itertools
+
+def main():
+    if len(sys.argv) < 4:
+        print("Not enough arguments! You need to input 3 arguments(url,subdomains input file, directories and files input file)!")
+        sys.exit(1)
+        
+if __name__ == '__main__':
+    main()

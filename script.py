a70cb1e20f5cb779469ea8823222984f6321dc96
NajatN <121221757+NajatN@users.noreply.github.com>
2023-03-21 22:58:30 +0200

    Read and store arguments

--- script.py
+++ script.py
@@ -9,5 +9,9 @@ def main():
         print("Not enough arguments! You need to input 3 arguments(url,subdomains input file, directories and files input file)!")
         sys.exit(1)
         
+    target = sys.argv[1]
+    subs_input_file = sys.argv[2]
+    dirs_input_file = sys.argv[3]
+        
 if __name__ == '__main__':
     main()
